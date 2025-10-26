d199ecbe35666910359176e437d62e40205aeb29
Christian Sum <christian.osum@gmail.com>
2025-10-26 00:40:28 -0600

    Extractor Init

---
+++ is-extractor-srv/src/main/java/com/td/extractor/IsExtractorSrvApplication.java
@@ -0,0 +1,13 @@
+package com.td.extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsExtractorSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsExtractorSrvApplication.class, args);
+    }
+
+}
---
+++ is-extractor-srv/src/test/java/com/td/extractor/IsExtractorSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsExtractorSrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

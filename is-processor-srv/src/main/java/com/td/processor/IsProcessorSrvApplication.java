12fc1e6e52d90c3c1430601979f16cb39501940a
Christian Sum <christian.osum@gmail.com>
2025-10-26 00:55:03 -0600

    Processor Init

---
+++ is-processor-srv/src/main/java/com/td/processor/IsProcessorSrvApplication.java
@@ -0,0 +1,13 @@
+package com.td.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsProcessorSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsProcessorSrvApplication.class, args);
+	}
+
+}

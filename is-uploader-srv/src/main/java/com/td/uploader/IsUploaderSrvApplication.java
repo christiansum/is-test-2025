763f71b851ef40c4b420f8759b809e8352e18033
Christian Sum <christian.osum@gmail.com>
2025-10-26 00:47:44 -0600

    Uploader init

---
+++ is-uploader-srv/src/main/java/com/td/uploader/IsUploaderSrvApplication.java
@@ -0,0 +1,13 @@
+package com.td.uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsUploaderSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsUploaderSrvApplication.class, args);
+	}
+
+}
---
+++ is-uploader-srv/src/test/java/com/td/uploader/IsUploaderSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td.uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsUploaderSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

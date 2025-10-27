138f52231966ec1a63d9057153090935f0f0efe0
Christian Sum <christian.osum@gmail.com>
2025-10-26 19:37:07 -0600

    RedisConfig for Services

---
+++ is-extractor-srv/src/main/java/com/td/extractor/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.td.extractor;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+	@Bean(name = "redisConnectionFactory")
+	public LettuceConnectionFactory redisConnectionFactory(Environment env) {
+		String host = env.getProperty("REDIS_HOST", "redis");
+		int port = Integer.parseInt(env.getProperty("REDIS_PORT", "6379"));
+		String pwd = env.getProperty("REDIS_PASSWORD", "");
+		var conf = new RedisStandaloneConfiguration(host, port);
+		if (pwd != null && !pwd.isBlank()) conf.setPassword(pwd);
+		return new LettuceConnectionFactory(conf);
+	}
+
+	@Bean
+	public RedisTemplate<String,String> redisTemplate(LettuceConnectionFactory cf) {
+		var t = new RedisTemplate<String,String>();
+		t.setConnectionFactory(cf);
+		return t;
+	}
+}
